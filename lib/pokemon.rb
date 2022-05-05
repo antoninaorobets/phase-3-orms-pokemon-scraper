3678c50dc293c2b30396946666f31d3dd18cb3f2
antoninaorobets <pani.antonina.don@gmail.com>
2022-05-05 10:55:12 -0400

    commit tree b4a730b6ebff159304a110bd1c9e06a49b7d7325

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
+require 'pry'
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    def initialize(name:, type:, db:, id: nil)
+        @name=name
+        @type = type
+        @id = id
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        # binding.pry
+        db.execute("INSERT INTO pokemon (name,type) VALUES (?,?)", name, type)
+       
+        # id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        db.results_as_hash = true
+        row = db.execute("SELECT * FROM pokemon WHERE id = ?",id)[0]
+        self.new(name: row["name"], type: row["type"], id: row["id"], db: db)
+        # binding.pry
+    end
 end
+
+# binding.pry
+# 0
